2ff2b3bbe7fef1a1d44d9661ea55584f07b7dea5
Ilya Khaprov <dead.trickster@gmail.com>
2017-04-16 00:33:38 +0300

    mix.exs

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Rebar3ArchivePlugin.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :rebar3_archive_plugin,
+     version: "0.0.1",
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  defp description do
+    """
+    Create Erlang Code archives with rebar3.
+    """
+  end
+
+  defp package do
+    [build_tools: ["rebar3"],
+     maintainers: ["Ilya Khaprov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/deadtrickster/rebar3_archive_plugin"},
+     files: ["src", "README.md", "LICENSE", "rebar.config"]]
+  end
+
+  defp deps do
+    [
+    ]
+  end
+end
